303f9d212cc25defa8e488f2304d169387824718
axytho <jojojo.jonas888@hotmail.com>
2019-11-26 15:57:32 +0100

    Stuff done today

---
+++ Week5/ofdm_channelest.m
@@ -0,0 +1,103 @@
+% Exercise session 4: DMT-OFDM transmission scheme
+close all;
+% Convert BMP image to bitstream
+fs= 16000;
+M=16;
+Nframe = 2002;
+SNR = 300;
+Lfilter = 400;
+Lprefix = 400;
+impulseresponseStruct = load('h.mat');
+h = impulseresponseStruct.h;
+H = fft(h);
+noiseStruct = load('noisePower.mat');
+noisePower = noiseStruct.noiseOut;
+noisePower = noisePower(1:end-1)';
+
+trainblock = randi([0, 1], (Nframe/2-1)*M, 1);
+trainrect = repmat(trainblock, 100, 1);
+size(trainrect);
+
+%The problem seems to be that some pixels are just widely off, while others
+% are actually almost fine, the BER with this h is 3 times higher than with
+% a channel of 1
+% The channel heavily surpresses anything above 2000 Hz which is 1/4
+% of the 8000 = pi so we should only code in the first 4th of the 
+% function, not beyond that.
+
+% QAM modulation
+qamTrain = qam_mod(trainrect, M);
+
+
+% OFDM modulation
+[ofdmStream, remainder] = ofdm_mod(qamTrain, Nframe, Lprefix);
+%[ofdmStream, badbits] = ofdm_qam(bitStream, b, Lprefix);
+
+% Channel
+
+noisyOfdmStream = awgn(ofdmStream,SNR);
+%h = rand(1, Lfilter);
+%h = [0.5, 0.6, 0.7, 0.3, 0.4, 0.7];
+Rx = filter(h, 1, noisyOfdmStream);
+
+%rxOfdmStream = noisyOfdmStream;
+% OFDM demodulatio
+
+[rxQamStream, hEstimated] = ofdm_demod(Rx, Nframe, remainder, Lprefix, qamTrain(1:(Nframe/2 - 1), :), true);
+
+%rxQamStream = ofdm_deqam(rxOfdmStream, b, badbits, Lprefix, h);
+
+% QAM demodulation
+rxBitStream = qam_demod(rxQamStream, M);
+%rxBitStream = rxQamStream;
+
+% Compute BER
+berTransmission = biterr(trainrect,rxBitStream); % Gray is best for constellation
+
+hChannel = h';
+HChannel = fft(hChannel);
+
+
+hEstimated = hEstimated(1:Lfilter)';
+HEstimated = fft(hEstimated);
+
+
+
+t = (1:size(hChannel, 1) )/fs;
+f = (1:size(HChannel, 1))*fs/Lfilter;
+tEst = (1:size(hEstimated, 2))/fs;
+fEst = (1:size(HEstimated, 2))*fs/Lfilter;
+
+hEstimated
+size(tEst)
+
+figure('name','Acoustic impulse and frequency response')
+
+subplot(2,1,1)
+plot(t, hChannel);
+title('Acoustic impulse response h')
+xlabel('t');
+ylabel('Acoustic impulse response h');
+
+subplot(2,1,2)
+plot(f, mag2db(abs(HChannel)));
+title('Acoustic impulse frequency response H')
+xlabel('f');
+ylabel('Acoustic impulse frequency response H');
+
+
+
+figure('name','Estimated channel impulse and frequency response')
+
+subplot(2,1,1)
+plot(tEst, hEstimated);
+title('Estimated channel impulse response h')
+xlabel('t');
+ylabel('Estimated channel impulse response h');
+
+subplot(2,1,2)
+plot(fEst, mag2db(abs(HEstimated)));
+title('Estimated channel frequency response H')
+xlabel('f');
+ylabel('Estimated channel frequency response H');
+
--- Week5/ofdm_mod.m
+++ Week5/ofdm_mod.m
@@ -2,13 +2,10 @@ function [OFDM, remainder] = ofdm_mod(QAM,N, varargin)
 switch nargin
     case 2
         preLength = 0;
-        freqbin = 1;
     case 3
         preLength = varargin{1};
-        freqbin = 1;
     case 4
         preLength = varargin{1};
-        %freqbin = varargin{2};
 end
 %code to make sure we can get an int dimelength
 dimLength = floor(length(QAM)/(N/2 - 1));
@@ -18,14 +15,7 @@ remainder = mod(length(QAM), (N/2 - 1));
 bitSequence = reshape(QAM(1:((N/2-1)*dimLength)), (N/2 - 1) , dimLength);
 % and add the remainder plus trailing zeros
 bitSequence = [bitSequence, [QAM(end - remainder + 1:end);zeros(N/2-1-remainder,1)]];
-%Sent = bitSequence(200:300,26)
-% if (remainder >0)
-%     %Don't do this, you should throw an error
-% %    ME = MException('MyComponent:chooseadifferentN', ...
-% %         'This is a bad value for N for the ofdm that you use');
-% %     throw(ME)
-% end
-%continue...
+
 total = [zeros(1, dimLength+(1)); bitSequence; zeros(1, dimLength+(1)); flip(conj(bitSequence), 1)];
 OFDMRECT = ifft(total, [], 1);
 OFDMPREFIXED = [OFDMRECT(end-preLength+1:end, :); OFDMRECT];
