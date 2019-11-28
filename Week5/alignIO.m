8d5586062c5eea46eb76528c7cccfc38af9992ad
Charlotte Van De Mieroop <r0653703@kuleuven.be>
2019-11-28 10:29:23 +0100

    week 5 finished

--- Week5/alignIO.m
+++ Week5/alignIO.m
@@ -1,6 +1,6 @@
-function out_aligned = alignIO(out, pulse)
+function out_aligned = alignIO(out, pulse, filterLength)
 [correlation, lags] = xcorr(out, pulse);
 [~, index] = max(correlation);
-lag = lags(index);
+lag = lags(index)+length(pulse)-20+5*filterLength;
 out_aligned = out(lag:end);
 end
